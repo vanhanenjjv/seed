dcf312da9e8c1af5ed3af5f16c29af55d081b4b1
Janne Vanhanen <janne.vanhanen@outlook.com>
2023-11-09 19:33:28 +0200

    WIP on tunking

---
+++ services/calculator/functions/sum/handler.ts
@@ -0,0 +1,27 @@
+import type { APIGatewayProxyHandlerV2 } from "aws-lambda";
+
+function parseOperands(body?: string): number[] | null {
+  if (!body) return null
+  try { return JSON.parse(body) } 
+  catch (e) { return null }
+}
+
+const reducer = (total: number, value: number) => total + value
+
+const handler: APIGatewayProxyHandlerV2 = async event => {
+  const operands = parseOperands(event.body)
+  
+  if (!operands) return { 
+    statusCode: 400, 
+    body: JSON.stringify({ error: "Invalid input" }) 
+  }
+
+  const sum = operands.reduce(reducer)
+
+  return { 
+    statusCode: 200, 
+    body: JSON.stringify({ result: sum }) 
+  }
+}
+
+export default handler
