ed1bf947b849ec741cf75d3243682d2ce8095ba7
Janne Vanhanen <janne.vanhanen@outlook.com>
2023-11-09 21:32:14 +0200

    WIP e2e tests

---
+++ tests/calculator.test.ts
@@ -0,0 +1,15 @@
+import { expect, test } from '@playwright/test'
+
+test.describe('Calculator', () => {
+  test('should sum two numbers', async ({ page }) => {
+    await page.goto('/')
+    await page.fill('data-test-id=input', '1 2')
+    await Promise.all([
+      page.waitForResponse(response => response.url().endsWith('/sum')),
+      page.click('data-test-id=calculate'),
+    ])
+    // NOTE: is there a race condition of not waiting for the SPA to update?
+    const product = await page.locator('data-test-id=product').inputValue()
+    expect(product).toEqual('3')
+  })
+})
